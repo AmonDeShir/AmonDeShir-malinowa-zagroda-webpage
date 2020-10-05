616792d73e2722ebcf1a032e15530f8aff6b1779
AmonDeShir <amonsir29@gmail.com>
2020-10-05 07:45:49 +0200

    create map component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "../components/header/Header";
+import Map from "../components/map/Map";
 import SmallGallery from "../components/smallGallery/SmallGallery";
 
 const Home = () => (
   <>
     <Header/>
     <SmallGallery/>
+    <Map/>
   </>
 );
 
