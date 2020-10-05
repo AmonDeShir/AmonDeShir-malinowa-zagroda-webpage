572d3081d558f1ecdaa17de694b0bfdf6720fe8d
AmonDeShir <amonsir29@gmail.com>
2020-10-05 08:35:19 +0200

    Add about us and contact section

---
+++ src/components/textblock/Textblock.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: ${({height}) => height ? height : "180px"};
+  background: ${({theme}) => theme.colors.lightMain};
+
+  text-align: center;
+`;
+
+const Title = styled.h2`
+  padding: 0;
+  margin: 0;
+
+  height: 50px;
+  width: 100%;
+  line-height: 50px;
+
+  font-style: normal;
+  font-weight: 300;
+  font-size: ${({theme}) => theme.fonts.phone.xl};
+`;
+
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-style: normal;
+  font-weight: 300;
+  font-size: ${({theme}) => theme.fonts.phone.m};
+
+  width: 100%;
+  height: calc(100% - 50px - 25px);
+`;
+
+const Textblock = ({ title, text, height }) => (
+  <Container height={height}>
+    <Title>{title}</Title>
+    <Text>{text}</Text>
+  </Container>
+);
+
+export default Textblock;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,29 @@ import React from "react"
 import Header from "../components/header/Header";
 import Map from "../components/map/Map";
 import SmallGallery from "../components/smallGallery/SmallGallery";
+import Textblock from "../components/textblock/Textblock";
 
 const Home = () => (
   <>
     <Header/>
     <SmallGallery/>
+    <Textblock
+      title="O nas"
+      text="Gospodarstwo ekologiczne Malinowa Zagroda powstała w 2005 roku, na początku zajmowaliśmy się uprawą malin i produkcją z niej przetworów. Nasza przygoda z serowarstwem rozpoczeła się w 2008 roku, od wyrobów z mleka koziego. DziimagesselectedImage zajmujy się głównie wyrobami z melka krowiego a wyroby kozie są dostępne tylko sezonowo."
+    />
+
     <Map/>
+    <Textblock
+      title="Kontakt"
+      text={
+        <>
+          Telefon: 512 945 451 <br/>
+          Email: kontakt@malinowa-zagroda.com <br/>
+          Adres: Wysoka, 52 59-170 Przemków
+        </>
+      }
+      height="120px"
+    />
   </>
 );
 
