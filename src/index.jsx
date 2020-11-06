61c6e7cecc1783b415d6933518f5ba735595f747
AmonDeShir <amonsir29@gmail.com>
2020-11-06 12:43:10 +0100

    Add styled-normalize
    Add styled-normalize to provide better browser support

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from "./styles/theme";
 
+ 
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize/>
     <GlobalStyle/>
     <ThemeProvider theme={theme}>
       <App />
